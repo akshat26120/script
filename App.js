5a4c305c0ab6935331b4e513418b69da8abcb48d
akshat26120 <26120akshtaravdiya@gmail.com>
2024-01-20 14:21:53 +0530

    restart

---
+++ App.js
@@ -0,0 +1,6 @@
+import React from "react"
+import  ReactDOM  from "react-dom/client"
+
+const nest = React.createElement("h1",{},"Rendered Successfully")
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(nest)
